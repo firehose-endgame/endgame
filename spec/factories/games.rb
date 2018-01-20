a3cbf22f8eb25e8d795d4af5bb6021a25b406d5f
Yoshi Moriyama <eyoshimoriyama@gmail.com>
2018-01-20 19:50:28 +0000

    Added update to games controller

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :game do
-    user_id 1
-    opponent_id nil
-    current_turn_player_id nil
-    winning_player_id nil
     name "Test"
     association :user
   end
