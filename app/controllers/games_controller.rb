1339508d1f39d0daaaeb3fd5400683405d0cd515
nerdycat99 <info@drysky.co.uk>
2018-01-22 14:43:57 +0000

    refactored ruby code to construct the board

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,17 @@
+
+module GamesHelper
+  
+  def cell_black(row, column)
+    if row % 2 == 0 && column % 2 == 0 || row % 2 != 0 && column % 2 != 0
+      return true
+    else
+      return false
+    end
+  end
+
+end
+
+
 class GamesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update]
 
