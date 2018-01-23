626b875dbfd2efa39ec5cc9491823a04f9463c13
nerdycat99 <info@drysky.co.uk>
2018-01-23 20:17:43 +0000

    method to determine colour pushed out to helpers.rb file

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,11 @@
 module GamesHelper
+  
+  def cell_black(row, column)
+    if row % 2 == 0 && column % 2 == 0 || row % 2 != 0 && column % 2 != 0
+      return true
+    else
+      return false
+    end
+  end
+
 end
