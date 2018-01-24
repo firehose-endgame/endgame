523ad34cd4f9ee70c0cc14c31a9c7b7035d078e8
nerdycat99 <info@drysky.co.uk>
2018-01-24 08:44:40 +0000

    board refactored code in helper module

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,11 +1,7 @@
 module GamesHelper
   
   def cell_black(row, column)
-    if row % 2 == 0 && column % 2 == 0 || row % 2 != 0 && column % 2 != 0
-      return true
-    else
-      return false
-    end
+    row % 2 == 0 && column % 2 == 0 || row % 2 != 0 && column % 2 != 0
   end
 
 end
