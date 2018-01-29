b3bbe6b9c7eb0a818c90684a2f455332d8fe7883
JosephFleetham <joseph.fleetham@gmail.com>
2018-01-29 19:59:16 +0000

    Refactored tests

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,22 +1,19 @@
 RSpec.describe King, type: :model do
 	describe "is_valid? method" do
     it "should check if king can move 1 space up" do
-    	game = FactoryBot.create(:game)
-      mover = FactoryBot.create(:king, game_id: game.id)
-      valid_move = mover.is_valid?(4, 5)
-      expect(valid_move).to eq true
+      mover = FactoryBot.create(:king)
+      is_valid = mover.is_valid?(4, 5)
+      expect(is_valid).to eq true
     end
     it "should check if king can move 1 space sideways" do
-    	game = FactoryBot.create(:game)
-      mover = FactoryBot.create(:king, game_id: game.id)
-      valid_move = mover.is_valid?(3, 4)
-      expect(valid_move).to eq true
+      mover = FactoryBot.create(:king)
+      is_valid = mover.is_valid?(3, 4)
+      expect(is_valid).to eq true
     end
     it "should check if king can move 1 space diagonally" do
-    	game = FactoryBot.create(:game)
-      mover = FactoryBot.create(:king, game_id: game.id)
-      valid_move = mover.is_valid?(3, 5)
-      expect(valid_move).to eq true
+      mover = FactoryBot.create(:king)
+      is_valid = mover.is_valid?(3, 5)
+      expect(is_valid).to eq true
     end
   end
 end
