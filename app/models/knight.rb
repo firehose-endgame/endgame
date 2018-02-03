cd12c638560d25e83cf126a0234d34d0516b89d6
Sam Gardiner <nekura.gesshoku@gmail.com>
2018-02-03 07:33:52 +0000

    set up knight is_valid? and tests

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,3 +1,17 @@
 class Knight < Piece
 
+	def is_valid?(new_x, new_y)
+		total_x = new_x - self.x_coordinate
+		total_x = total_x.abs
+		total_y = new_y - self.y_coordinate
+		total_y = total_y.abs
+		if total_x == 1 && total_y == 3
+			return true
+		elsif total_x == 3 && total_y == 1
+			return true
+		else
+			return false
+		end
+	end
+
 end
