2b568987b2dcae0d9331d7be7d004ad9bcec200b
Yoshi Moriyama <eyoshimoriyama@gmail.com>
2018-02-04 06:39:06 +0000

    Updated queen is_valid

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,7 +1,6 @@
 class Queen < Piece
 
   def is_valid?(new_x, new_y)
-    return super
     return false unless super
     return is_valid_diagonal?(new_x, new_y) || is_valid_straight?(new_x, new_y)
   end
