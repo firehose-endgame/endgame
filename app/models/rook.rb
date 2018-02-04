fb8f956e2c02e6e611f4f5f8733e7e793072ccbd
nerdycat99 <info@drysky.co.uk>
2018-02-04 17:27:06 +0000

    refactored rook horizontal/vertical move checks

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -2,7 +2,15 @@ class Rook < Piece
 
     def is_valid?(new_x, new_y)
     	return false unless super
-    	(new_x == self.x_coordinate || new_y == self.y_coordinate)? true : false
+    	vertical_move(new_x) || horizontal_move(new_y)
+    end
+
+    def vertical_move(new_x)
+    	return new_x == self.x_coordinate
+    end
+
+    def horizontal_move(new_y)
+    	return new_y == self.y_coordinate
     end
 
 end
