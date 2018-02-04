041788e3edea834beee4f4e5568c92e75ef860b8
Sam Gardiner <nekura.gesshoku@gmail.com>
2018-02-04 17:54:45 +0000

    made recommeded changes

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,13 +1,10 @@
 class Knight < Piece
 
 	def is_valid?(new_x, new_y)
-		total_x = new_x - self.x_coordinate
-		total_x = total_x.abs
-		total_y = new_y - self.y_coordinate
-		total_y = total_y.abs
-		if total_x == 1 && total_y == 3
-			return true
-		elsif total_x == 3 && total_y == 1
+		return false if off_board?(new_x, new_y)
+		distance_x = (new_x - self.x_coordinate).abs
+		distance_y = (new_y - self.y_coordinate).abs
+		if distance_x == 1 && distance_y == 2 || distance_x == 2 && distance_y == 1
 			return true
 		else
 			return false
--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe Knight, type: :model do
 	describe "is_valid? method" do
     it "checks for valid moves" do
       mover = FactoryBot.create(:knight)
-      valid_move_1 = mover.is_valid?(1, 3)
-      valid_move_2 = mover.is_valid?(5, 7)
+      valid_move_1 = mover.is_valid?(2, 3)
+      valid_move_2 = mover.is_valid?(5, 6)
       expect(valid_move_1).to eq true
       expect(valid_move_2).to eq true
     end
