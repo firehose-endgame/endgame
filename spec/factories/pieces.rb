3cd678afd0db7a1442d0e794a3f3e09d9ec040a7
JosephFleetham <joseph.fleetham@gmail.com>
2018-02-06 18:32:37 +0000

    Fixed typo

--- spec/factories/pieces.rb
+++ spec/factories/pieces.rb
@@ -8,13 +8,24 @@ FactoryBot.define do
     selected false
     association :game
   end
-  factory :king, class: King, parent: :piece do
-    type "King"
-  end
-  factory :queen, class: Queen, parent: :piece do
-    type "Queen"
-  end
-  factory :bishop, class: Bishop, parent: :piece do
-    type "Bishop"
-  end
+    factory :king, class: King, parent: :piece do
+      type "King"
+    end
+    factory :queen, class: Queen, parent: :piece do
+      type "Queen"
+    end
+
+    factory :bishop, class: Bishop, parent: :piece do
+      type "Bishop"
+    end
+    factory :pawn, class: Pawn, parent: :piece do
+      type "Pawn"
+    end
+    factory :rook, class: Rook, parent: :piece do
+      type "Rook"
+    end
+    factory :knight, class: Knight, parent: :piece do
+      type "Knight"
+
+    end
 end
