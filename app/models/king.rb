b725701d7956c2fb570279d34add969c0e3863e0
Sam Gardiner <nekura.gesshoku@gmail.com>
2018-02-15 00:05:09 +0000

    mostly working. The rook movement isn't working right yet, and needs to recognize obstructions

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,8 +1,29 @@
 class King < Piece
 
   def is_valid?(new_x, new_y)
-    return false unless super 
+    return false unless super
+    if moved == false && new_y == y_coordinate && (x_coordinate - new_x).abs == 2
+      if x_coordinate - new_x == 2
+        castle_rook = game.pieces.find_by(x_coordinate: 1, y_coordinate: y_coordinate, moved: false)
+        return true unless castle_rook.nil?
+      else
+        castle_rook = game.pieces.find_by(x_coordinate: 8, y_coordinate: y_coordinate, moved: false)
+        return true unless castle_rook.nil?
+      end
+    end
     return (x_coordinate - new_x).abs <=1 && (y_coordinate - new_y).abs <=1
+  end
+  def move_to(x, y)
+    if (x_coordinate - x).abs == 2
+      if x_coordinate - x == 2
+        castle_rook = game.pieces.find_by(x_coordinate: 1, y_coordinate: y_coordinate, moved: false)
+        castle_rook.move!(x + 1, y)
+      else
+        castle_rook = game.pieces.find_by(x_coordinate: 8, y_coordinate: y_coordinate, moved: false)
+        castle_rook.move!(x - 1, y)
+      end
+    end
+    super
   end
 
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
+  
 	describe "is_valid? method" do
+    <<-DOC
     it "should check if king can move 1 space up" do
       mover = FactoryBot.create(:king)
       mover.reload
@@ -24,6 +26,46 @@ RSpec.describe King, type: :model do
       king.move!(4, 5)
       is_valid = king.is_valid?(4, 6)
       expect(is_valid).to eq false
+    end
+DOC
+    it "should see castling as a valid move" do
+      king = FactoryBot.create(:king, x_coordinate: 5, y_coordinate: 4)
+      rook = FactoryBot.create(:rook, x_coordinate: 1, y_coordinate: 4, game: king.game)
+      valid_move = king.is_valid?(3, 4)
+      expect(valid_move).to eq true
+    end
+
+    it "should stop castling if the rook has been moved" do
+      king = FactoryBot.create(:king, x_coordinate: 5, y_coordinate: 4)
+      rook = FactoryBot.create(:rook, x_coordinate: 1, y_coordinate: 4, game: king.game, moved: true)
+      valid_move = king.is_valid?(3, 4)
+      expect(valid_move).to eq false
+    end
+
+    it "should stop castling if the king has been moved" do
+      king = FactoryBot.create(:king, x_coordinate: 5, y_coordinate: 4, moved: true)
+      rook = FactoryBot.create(:rook, x_coordinate: 1, y_coordinate: 4, game: king.game)
+      valid_move = king.is_valid?(3, 4)
+      expect(valid_move).to eq false
+    end
+  end
+
+  describe "move_to method" do
+    it "should be able to castle to the left" do
+      king = FactoryBot.create(:king, x_coordinate: 5, y_coordinate: 4)
+      rook = FactoryBot.create(:rook, x_coordinate: 1, y_coordinate: 4, game: king.game)
+      king.move_to(3, 4)
+      byebug
+      expect(king.x_coordinate).to eq 3
+      expect(rook.x_coordinate).to eq 4
+    end
+
+    it "should be able to castle to the right" do
+      king = FactoryBot.create(:king, x_coordinate: 5, y_coordinate: 4)
+      rook = FactoryBot.create(:rook, x_coordinate: 8, y_coordinate: 4, game: king.game)
+      king.move_to(7, 4)
+      expect(king.x_coordinate).to eq 7
+      expect(rook.x_coordinate).to eq 6
     end
   end
 end
