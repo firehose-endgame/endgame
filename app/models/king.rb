400eb5d5db041fbb201a2489699c6374bc8dcb62
Sam Gardiner <nekura.gesshoku@gmail.com>
2018-02-23 22:50:23 +0000

    refactors

--- app/models/king.rb
+++ app/models/king.rb
@@ -2,7 +2,7 @@ class King < Piece
 
   def is_valid?(new_x, new_y)
     return false unless super
-    if moved == false && new_y == y_coordinate && (x_coordinate - new_x).abs == 2
+    if !moved && castle_move?(new_x, new_y)
       if x_coordinate - new_x == 2
         return false if is_obstructed?(1 , new_y)
         castle_rook = game.pieces.find_by(x_coordinate: 1, y_coordinate: y_coordinate, moved: false)
@@ -15,6 +15,7 @@ class King < Piece
     end
     return (x_coordinate - new_x).abs <=1 && (y_coordinate - new_y).abs <=1
   end
+
   def move_to(x, y)
     if (x_coordinate - x).abs == 2
       if x_coordinate - x == 2
@@ -28,4 +29,11 @@ class King < Piece
     super
   end
 
+  def castle_move?(new_x, new_y)
+    if new_y == y_coordinate && (x_coordinate - new_x).abs == 2
+      return true
+    else
+      return false
+    end
+  end
 end
